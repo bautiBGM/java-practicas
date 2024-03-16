7bf846b651c677db6e3a44a61c269411750ec2cb
Bautista Gayoso <92990567+bautiBGM@users.noreply.github.com>
2024-03-16 01:39:30 -0300

    Update and rename Ejercicio_39.java to ManejoDeCiclos_39.java

--- src/thp/ejercicios/ManejoDeCiclos_39.java
+++ src/thp/ejercicios/ManejoDeCiclos_39.java
@@ -2,7 +2,7 @@ package thp.ejercicios;
 
 import java.util.Scanner;
 
-public class Ejercicio_39 {
+public class ManejoDeCiclos_39 {
 
 	public static Scanner input = new Scanner(System.in);
 	static final int MIN_JUGADORES = 2;
