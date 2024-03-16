7d7a4be1aa0c249340ef551f1652dea66c37ce32
Bautista Gayoso <92990567+bautiBGM@users.noreply.github.com>
2024-03-16 01:35:23 -0300

    Update and rename Ejercicio_25.java to ManejoDeCiclos_25.java

--- src/thp/ejercicios/ManejoDeCiclos_25.java
+++ src/thp/ejercicios/ManejoDeCiclos_25.java
@@ -1,7 +1,7 @@
 package thp.ejercicios;
 
 import java.util.Scanner;
-public class Ejercicio_25 {
+public class ManejoDeCiclos_25 {
 	public static Scanner input = new Scanner (System.in);
 	public static void main(String[] args) {
 
