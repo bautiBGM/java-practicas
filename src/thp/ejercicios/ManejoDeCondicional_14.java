3a16ab11afeb3ef04afdbca8702c395fdd219e03
Bautista Gayoso <92990567+bautiBGM@users.noreply.github.com>
2024-03-16 01:29:31 -0300

    Update and rename Ejercicio_14.java to ManejoDeCondicional_14.java

--- src/thp/ejercicios/ManejoDeCondicional_14.java
+++ src/thp/ejercicios/ManejoDeCondicional_14.java
@@ -1,7 +1,7 @@
 package thp.ejercicios;
 
 import java.util.Scanner;
-public class Ejercicio_14 {
+public class ManejoDeCondicional_14 {
 
 	public static Scanner entrada = new Scanner (System.in);
 	public static void main(String[] args) {
